188a5ae205b7cb0c2afd9b9c44f7e2c6015e992d
willianw <willianwernerangelodacosta@gmail.com>
2016-04-30 18:30:49 -0300

    Início da batalha
    Alteração de array para ArrayList na organização dos pokemóns.
    Falta criar a função hasNext()

--- src/Battle.java
+++ src/Battle.java
@@ -4,10 +4,11 @@ public class Battle {
     Scanner scan = new Scanner(System.in);
     Jogador j1, j2;
     
-    public Battle(){
+    public Battle() throws Exception{
         System.out.println("Batalha!");
         j1 = new Jogador(1);
         j2 = new Jogador(2);
+        
     }
     
 }
--- src/Jogador.java
+++ src/Jogador.java
@@ -1,25 +1,42 @@
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Scanner;
 
 public class Jogador {
     static final int MAXPOKEMONS = Main.MAXPOKEMONS;
     Scanner scan = new Scanner(System.in);
     String nome;
-    Pokemon[] campo;
+    Collection <Pokemon> campo;
+    Boolean desistido;
+    int numero;
     
     public void setNome(String nome){
         this.nome = nome;
     }
     
     public Jogador(int n) throws Exception{
-        System.out.print("Digite o nome do jogador #" + n + ": ");
+        this.desistido = false;
+        this.numero = n;
+        cadastrarJogador();
+        cadastrarPokemons();
+    }
+    
+    private void cadastrarJogador(){
+        System.out.print("Digite o nome do jogador #" + this.numero + ": ");
         this.nome = scan.next();
-        campo = new Pokemon[MAXPOKEMONS];
+    }
+    
+    private void cadastrarPokemons() throws Exception{
+        campo = new ArrayList<Pokemon>();
         for(int i = 0; i < MAXPOKEMONS; i++){
             Pokedex.apresentar();
             System.out.print("["+i+"/"+MAXPOKEMONS+"] Escolha um pokemón: ");
-            campo[i] = new Pokemon(Pokedex.getHashPokemon(Integer.parseInt(scan.next())));
+            campo.add(new Pokemon(Pokedex.getHashPokemon(Integer.parseInt(scan.next()))));
         }
     }
     
+    public void jogada(){
+        System.out.println("Vez de " + nome + ": ");
+    }
 }
